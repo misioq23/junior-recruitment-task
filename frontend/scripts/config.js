bb3af45d27ca8872fb420119b475fe7ca15dbcc6
misioq23 <mi23chal@gmail.com>
2019-05-16 13:36:30 +0200

    Config added

---
+++ frontend/scripts/config.js
@@ -0,0 +1,10 @@
+const elements = {
+	todoList: document.querySelector('.todo__list'),
+	addBtn: document.querySelector('.todo__add-btn'),
+	addInput: document.querySelector('.todo__task-input'),
+};
+const elementStrings = {
+	trash: 'todo__del-btn',
+	complete: 'todo__task-checkbox',
+};
+export { elements, elementStrings };
