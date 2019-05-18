1c4ecaf3429c719837cf60f76357753ceaf764e0
misioq23 <mi23chal@gmail.com>
2019-05-19 00:15:32 +0200

    Moved slash

--- frontend/scripts/model/Tasks.js
+++ frontend/scripts/model/Tasks.js
@@ -3,14 +3,14 @@ class Tasks {
 	constructor(address) {
 		// URL of database
 		this.address = address;
-		this.category = '/todos';
+		this.category = 'todos';
 	}
 	// Method returns Promise with JSON parsed data object or Error
 	getTasks() {
 		return new Promise((resolve, reject) => {
 			const xhr = new XMLHttpRequest();
 
-			xhr.open('GET', `${this.address}${this.category}`, true);
+			xhr.open('GET', `${this.address}/${this.category}`, true);
 
 			xhr.addEventListener('load', () => {
 				if (xhr.status === 200) {
