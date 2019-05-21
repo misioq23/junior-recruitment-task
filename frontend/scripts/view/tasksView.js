ce5e23065378a50446e9a768aef34bf22dd9fe2d
misioq23 <mi23chal@gmail.com>
2019-05-21 20:28:07 +0200

    added documentation to taskView

--- frontend/scripts/view/tasksView.js
+++ frontend/scripts/view/tasksView.js
@@ -1,20 +1,30 @@
-import { elementStrings, elements } from '../config';
+import { elements, elementStrings } from '../config';
 
-// Function clears every element inside todo__list
+/**
+ * Removes all tasks from DOM
+ * @return {undefined}
+ */
 const clearList = () => {
 	const range = document.createRange();
 	range.selectNodeContents(elements.todoList);
 	range.deleteContents();
 };
 
-// Function to delete only one task
-const deleteTask = (id) => {
-	document.querySelector(`[data-id="${id}"]`).remove();
+/**
+ * Finds task by id and removes it from DOM
+ * @param {number} taskID task id number
+ * @return {undefined}
+ */
+const deleteTask = (taskID) => {
+	document.querySelector(`[data-id="${taskID}"]`).remove();
 };
 
-// Function returns markup of one todo__task
-const showTask = (task) => {
-	// changes Finished string into number (can be read like boolean)
+/**
+ * Creates task markup
+ * @param {object} task object
+ * @return {string} task's markup
+ */
+const makeTaskMarkup = (task) => {
 	const taskFinished = parseInt(task.Finished);
 	return `
 		<li class="todo__task ${taskFinished ? 'todo__task--complete' : ''}" data-id="${task.ID}">
@@ -29,7 +39,22 @@ const showTask = (task) => {
 		</li>`;
 };
 
-// Function change task look and add disable to delete button
+/**
+ * Iterates the tasks, making one markup string and inserts it into DOM
+ * @param {array} tasks array with task objects
+ * @return {undefined}
+ */
+const renderTasks = (tasks) => {
+	const markup = tasks.map(makeTaskMarkup).join(' ');
+	elements.todoList.insertAdjacentHTML('beforeend', markup);
+};
+
+/**
+ * Changes task appearance by toggle taskComplete class and disabling delete button
+ * @param {number} taskID id of clicked task
+ * @param {boolean} isChecked whether task is completed
+ * @return {undefined}
+ */
 const toggleCompleteUI = (taskID, isChecked) => {
 	document.querySelector(`[data-id="${taskID}"]`).classList.toggle(elementStrings.taskComplete);
 	if (isChecked) {
@@ -39,10 +64,4 @@ const toggleCompleteUI = (taskID, isChecked) => {
 	}
 };
 
-// Function join all todo__tasks and insert it into todo__list
-const renderTasks = (tasks) => {
-	const markup = tasks.map(showTask).join(' ');
-	elements.todoList.insertAdjacentHTML('beforeend', markup);
-};
-
-export { renderTasks, clearList, deleteTask, toggleCompleteUI };
+export { clearList, deleteTask, renderTasks, toggleCompleteUI };
